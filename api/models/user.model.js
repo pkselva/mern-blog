62063c1e0669f843a1e03267221c1a52089f4035
pkselva <karthickselvan07@gmail.com>
2025-04-21 15:46:36 +0530

    Create User Model

---
+++ api/models/user.model.js
@@ -0,0 +1,25 @@
+import { model, Schema } from "mongoose";
+
+
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        trim: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+const User = model("User", userSchema);
+
+module.exports = User;
